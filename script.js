14ecd8b50b9c8c422dba9e6429016fad0b2ff1c9
Aidan Liu <lyz784125253@gmail.com>
2021-05-02 12:45:39 -0700

    img

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ const img = new Image(); // used to load image from <input> and draw to canvas
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
-  img.src = URL.createObjectURL(img);
   //document.querySelector("[type='reset']").disabled = false;
   const canvas = document.getElementById("user-image");
-  //const ctx = canvas.getContext('2d');
-  //ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear canvas
-  //ctx.fillStyle = 'black';
-  //ctx.fillRect(0, 0, canvas.width, canvas.height);    // fill canvas with black
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear canvas
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);    // fill canvas with black
   //document.getElementById("generate-meme").reset();   // clear the form
-  //var img = document.getElementById("image-input");
+  document.getElementById("image-input").src = URL.createObjectURL(img);
   // draw image with proper dimension
-  //let imgdata = getDimmensions(canvas.width, canvas.height, img.clientWidth, img.clientHeight);
-  //ctx.drawImage(img, imgdata.startX, imgdata.startY, imgdata.width, imgdata.height);
+  let imgdata = getDimmensions(canvas.width, canvas.height, img.clientWidth, img.clientHeight);
+  ctx.drawImage(img, imgdata.startX, imgdata.startY, imgdata.width, imgdata.height);
 
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
