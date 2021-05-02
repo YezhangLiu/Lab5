f5e4f64c87868eb21c14ba26f9e3c909237e4876
Aidan Liu <lyz784125253@gmail.com>
2021-05-02 16:05:09 -0700

    55

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // script.js
-
 const img = new Image(); // used to load image from <input> and draw to canvas
-img.src = document.getElementById("image-input").value;
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO 
@@ -14,13 +12,12 @@ img.addEventListener('load', () => {
   // draw image with proper dimension
   //let imgdata = getDimmensions(canvas.width, canvas.height, img.clientWidth, img.clientHeight);
   //ctx.drawImage(img, imgdata.startX, imgdata.startY, imgdata.width, imgdata.height);
-  document.querySelector().disabled = false;
-  
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
 });
+img.src = document.getElementById("image-input").src
 // change image
 const inputImg = document.getElementById("image-input");
 inputImg.addEventListener("change", handleFiles, false);
