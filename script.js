54dadf8f786587e316b9de69db42debae3c75a2f
Aidan Liu <lyz784125253@gmail.com>
2021-05-02 11:59:38 -0700

    reset

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const img = new Image(); // used to load image from <input> and draw to canvas
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
-  const canvas = document.getElementById("user-image");
-  const ctx = canvas.getContext('2d');
+  document.querySelector("[type='reset']").disabled = false;
+  //const canvas = document.getElementById("user-image");
+  //const ctx = canvas.getContext('2d');
   //ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear canvas
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);    // fill canvas with black
+  //ctx.fillStyle = 'black';
+  //ctx.fillRect(0, 0, canvas.width, canvas.height);    // fill canvas with black
   //document.getElementById("generate-meme").reset();   // clear the form
   //var img = document.getElementById("image-input");
   // draw image with proper dimension
