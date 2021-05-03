7f601b3c51994d9583b7fde92803fd74217959d8
Aidan Liu <lyz784125253@gmail.com>
2021-05-02 22:49:38 -0700

    5456

--- script.js
+++ script.js
@@ -13,15 +13,17 @@ selection.disabled = false;
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 image.addEventListener('change', function (event) {
-  alert(event.target.files[0].name);
+  
   ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear canvas
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);    // fill canvas with black
   //draw image with proper dimension
   img.src = URL.createObjectURL(event.target.files[0]);
   img.alt = event.target.files[0].name;   // change alt name
-  let imgdata = getDimmensions(canvas.width, canvas.height, img.clientWidth, img.clientHeight);
-  ctx.drawImage(img, imgdata.startX, imgdata.startY, imgdata.width, imgdata.height);
+  alert(img.alt);
+  ctx.drawImage(img, 0, 0);
+  //let imgdata = getDimmensions(canvas.width, canvas.height, img.clientWidth, img.clientHeight);
+  //ctx.drawImage(img, imgdata.startX, imgdata.startY, imgdata.width, imgdata.height);
 });
 
 // submit button
